62dc6a5a6b50bf48e17b6e35449d19283d11f001
Naveen Kumar Reddy Kota <naveenkumar.kota@rampgroup.com>
2022-07-05 05:09:08 -0700

    updated css and responsive for all platforms

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,51 +1,51 @@
 import {
-    IonButton,
-    IonContent,
-    IonHeader,
-    IonItem,
-    IonPage,
-    IonTitle,
-    IonToolbar,
-    IonGrid,
-    IonImg,
-    IonRow,
-    IonCol,
-    IonLabel
-  } from "@ionic/react";
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonImg,
+  IonRow,
+  IonCol,
+  IonLabel
+} from "@ionic/react";
 import { Link } from "react-router-dom";
-  import "./LandingPage.css";
-  const LandingPage = () => {
-    
-  
-    return (
-      <IonPage>
-        <IonContent className="const">
-          <IonGrid>
-            <IonRow className="signup-row">
+import "./LandingPage.css";
+const LandingPage = () => {
 
-            <IonImg src="assets/icon/Group.svg" alt="" className="img" />
-      
-              <IonLabel className="plan">PlanTravel</IonLabel>
-           
-            </IonRow>
 
-                <IonRow className="text-row">
+  return (
+    <IonPage>
+      <IonContent className="const">
+        <IonGrid className="grid8">
+          <IonRow className="signup-row">
+
+            <IonImg src="assets/icon/Group.svg" className="img" alt="" />
+
+            <IonLabel className="plan">planTravel</IonLabel>
+
+          </IonRow>
+
+          <IonRow className="text-row">
             <IonLabel >Search less,</IonLabel>
-            </IonRow>
+          {/* </IonRow>
 
-            <IonRow className="text-row1">
-              <IonLabel >travel more</IonLabel>
-              </IonRow>
+          <IonRow className="text-row1"> */}
+            <IonLabel >travel more</IonLabel>
+          </IonRow>
 
-            <IonRow className="text-row2">
+          <IonRow className="text-row2">
             <IonLabel >Great experience at backpacker prices</IonLabel>
-            </IonRow>
+          </IonRow>
 
-            <IonRow className="create-acnt-btn">
-            <IonButton expand="full"size="default"fill="solid"color="green"routerLink="/signup" className="plan3"> CREATE AN ACCOUNT</IonButton>
-           
-            </IonRow>
-           <IonRow className="login-acnt-btn">
+          <IonRow className="create-acnt-btn">
+            <IonButton expand="full" size="default" fill="solid" color="green" routerLink="/signup" className="plan3"> CREATE AN ACCOUNT</IonButton>
+
+          </IonRow>
+          <IonRow className="login-acnt-btn">
             <IonButton
               expand="full"
               size="default"
@@ -56,19 +56,14 @@ import { Link } from "react-router-dom";
             >
               LOG IN
             </IonButton>
-            </IonRow>
-            <IonRow className="plan5">
-             <IonLabel >By creating an account you agree to our</IonLabel>
-           </IonRow>
-           <IonRow className="lp-last-text" >
-            
-              <IonLabel ><Link>Terms & Conditions</Link> and agree to <Link>Privacy Policy</Link> </IonLabel>
-             
-            </IonRow>
-            </IonGrid>
-          </IonContent>
-      </IonPage>
-    );
-  };
-  export default LandingPage;
-  
+          </IonRow>
+          <IonRow className="plan5">
+            <IonLabel >By creating an account you agree to our</IonLabel>
+            <IonLabel ><Link to="/">Terms & Conditions</Link> and agree to <Link to="/">Privacy Policy</Link> </IonLabel>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+export default LandingPage;
