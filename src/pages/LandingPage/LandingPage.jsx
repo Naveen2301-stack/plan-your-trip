529fb4d9c16cadc433ad3e0b22dc06a7d21a08fa
Naveen Kumar Reddy Kota <naveenkumar.kota@rampgroup.com>
2022-07-05 04:04:56 -0700

    layout edit with grid system

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -6,7 +6,13 @@ import {
     IonPage,
     IonTitle,
     IonToolbar,
+    IonGrid,
+    IonImg,
+    IonRow,
+    IonCol,
+    IonLabel
   } from "@ionic/react";
+import { Link } from "react-router-dom";
   import "./LandingPage.css";
   const LandingPage = () => {
     
@@ -14,17 +20,32 @@ import {
     return (
       <IonPage>
         <IonContent className="const">
-            <img src="assets/icon/Group.svg" alt="" />
-            <p className="plan">PlanTravel</p>
-            <h1 className="plan1">
-              Search less,
-              <br />
-              Travel more!
-            </h1>
-            <p className="plan2">Great experience at backpacker prices</p>
-            <IonButton expand="full"size="default"fill="solid"color="green"className="plan3" routerLink="/signup"> CREATE AN ACCOUNT</IonButton>
-            <br />
+          <IonGrid>
+            <IonRow className="signup-row">
+
+            <IonImg src="assets/icon/Group.svg" alt="" className="img" />
+      
+              <IonLabel className="plan">PlanTravel</IonLabel>
            
+            </IonRow>
+
+                <IonRow className="text-row">
+            <IonLabel >Search less,</IonLabel>
+            </IonRow>
+
+            <IonRow className="text-row1">
+              <IonLabel >travel more</IonLabel>
+              </IonRow>
+
+            <IonRow className="text-row2">
+            <IonLabel >Great experience at backpacker prices</IonLabel>
+            </IonRow>
+
+            <IonRow className="create-acnt-btn">
+            <IonButton expand="full"size="default"fill="solid"color="green"routerLink="/signup" className="plan3"> CREATE AN ACCOUNT</IonButton>
+           
+            </IonRow>
+           <IonRow className="login-acnt-btn">
             <IonButton
               expand="full"
               size="default"
@@ -35,12 +56,16 @@ import {
             >
               LOG IN
             </IonButton>
-            <p className="plan5">By creating an account you agree to our</p>
-            <br />
-            <p className="plan6">
-              <u className="plan7">Terms & Conditions </u> and agree to
-              <u className="plan7"> Privacy Policy </u>
-            </p>
+            </IonRow>
+            <IonRow className="plan5">
+             <IonLabel >By creating an account you agree to our</IonLabel>
+           </IonRow>
+           <IonRow className="lp-last-text" >
+            
+              <IonLabel ><Link>Terms & Conditions</Link> and agree to <Link>Privacy Policy</Link> </IonLabel>
+             
+            </IonRow>
+            </IonGrid>
           </IonContent>
       </IonPage>
     );
--- src/pages/Tab1.jsx
+++ src/pages/Tab1.jsx
@@ -1,22 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonImg, IonLabel, IonSearchbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1 = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonImg src="assets/icon/Arrow.svg" alt="" className='img3'/>
+      <IonLabel className='home1'>Hi Vecna!</IonLabel>
+      <IonLabel className='home2'>Discover</IonLabel>
+      <IonSearchbar placeholder="search for places" className='sb' showCancelButton="never"></IonSearchbar>
       </IonContent>
     </IonPage>
   );
